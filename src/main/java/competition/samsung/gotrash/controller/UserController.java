c1d01e1fda21874f6945e5b3b48ffe701396bfc0
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-08-19 07:48:46 +0700

    add guest

--- src/main/java/competition/samsung/gotrash/controller/UserController.java
+++ src/main/java/competition/samsung/gotrash/controller/UserController.java
@@ -5,21 +5,42 @@ import competition.samsung.gotrash.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigInteger;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api")
 @AllArgsConstructor
 public class UserController {
     private UserService userService;
 
-    @GetMapping("/")
+    @GetMapping("/users")
     public List<User> findAll(){
         return userService.findAll();
     }
 
-    @PostMapping("/")
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Integer id){
+        return userService.findById(id);
+    }
+
+    @PostMapping("/user/add")
     public User save(@RequestBody User user){
+        return userService.save(user);
+    }
+
+    @PostMapping("/user/addGuest")
+    public User saveGuest(){
+        User user = new User(1,
+                "dummy",
+                "dummy123",
+                "dummy@gmail.com",
+                "http://example.com/images/dummy.png",
+                BigInteger.valueOf(150),
+                LocalDateTime.now(),
+                LocalDateTime.now());
+
         return userService.save(user);
     }
 }
