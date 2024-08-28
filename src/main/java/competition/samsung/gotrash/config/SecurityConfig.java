6d2693a5f8b5ae4269dc3341afa0d8069b957459
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-08-28 21:33:32 +0700

    feat : spring security & add docker compose to run program locally

---
+++ src/main/java/competition/samsung/gotrash/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package competition.samsung.gotrash.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**").allowedOrigins("*");
+    }
+
+}
